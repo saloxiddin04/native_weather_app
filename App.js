c6b21c064c96569d9d2f0971bcaf0d0a448a6ec2
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-01-31 12:39:11 +0500

    start request api

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, StyleSheet} from 'react-native';
 import Loader from "./components/loader";
+import Weather from "./components/weather";
+import {useEffect, useState} from "react";
+import * as Location from 'expo-location'
 
 export default function App() {
+  
+  const [loader, setLoader] = useState(true)
+  
+  const getLocation = async () => {
+    try {
+      const {status} = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied!');
+        return;
+      }
+      
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({})
+    } catch (e) {
+      Alert.alert("I can't find your current location, so bad ):")
+    }
+  }
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false)
+      getLocation().then(r => r)
+    }, 2000)
+  }, []);
+  
   return (
-    <View>
-      <Loader/>
-      <StatusBar hidden={false} />
-    </View>
-  );
+    loader ? <Loader/> : <>
+      <Weather/>
+      <StatusBar/>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
